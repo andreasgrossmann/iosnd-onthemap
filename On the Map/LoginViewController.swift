594992f6a91a78684bbc6e8258fc7b8624d9088e
andreasgrossmann <grossmann.andreas@gmail.com>
2016-10-04 13:21:13 +0200

    add view controllers

---
+++ On the Map/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by Andreas on 25/09/2016.
+//  Copyright © 2016 Andreas. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
