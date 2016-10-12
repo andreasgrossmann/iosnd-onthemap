4de3ac96354f80ab7f3f3a17750c2fbac9cbaefd
andreasgrossmann <grossmann.andreas@gmail.com>
2016-10-12 11:17:59 +0100

    add map and table view

---
+++ On the Map/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  On the Map
+//
+//  Created by Andreas on 12/10/16.
+//  Copyright © 2016 Andreas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
